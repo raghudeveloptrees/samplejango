9843db5ecbf8f0f8077b4f3832bb8a2c56523e29
raghudeveloptrees <raghudeveloptrees@gmail.com>
2024-03-14 18:06:05 +0530

    allowed hosts

--- jango1/settings.py
+++ jango1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4o71!n7t+6!og@*kc9_#cjvuz%_(t=+z+=%4rj(x11j(f!k7!z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
