fd5a416884391a5bce988f32b79a2670c9011dbd
raghudeveloptrees <pubgjanacc5@gmail.com>
2024-07-02 12:29:45 +0530

    sync

--- runserver.sh
+++ runserver.sh
@@ -3,6 +3,8 @@
 cd /var/lib/jenkins/workspace/samplejango
 source antenv/bin/activate
 python3 manage.py makemigrations
-python3 manage.py migrate
-gunicorn jango1.wsgi:application -b 0.0.0.0:8000
+python3 manage.py migrate --run-syncdb
+sudo service gunicorn restart
+sudo service nginx restart
+#gunicorn jango1.wsgi:application -b 0.0.0.0:8000
 #python3 manage.py runserver 0.0.0.0:8000
