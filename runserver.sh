b834f6807b6ae3bafea3de4ef2c4e1cea3cccf31
raghudeveloptrees <pubgjanacc5@gmail.com>
2024-07-02 11:55:54 +0530

    runserver

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/samplejango
+cd /var/lib/jenkins/workspace/samplejango
 python3 manage.py makemigrations
 python3 manage.py migrate
 gunicorn jango1.wsgi:application -b localhost:8000 &
